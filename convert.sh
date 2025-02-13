5dc8f6a004d25ff4e4fab3b07bf1ca42a6e08c7a
Aleksandr Andreev <aleksandr.andreev@gmail.com>
2025-02-13 09:33:54 +0100

    Initial commit

---
+++ convert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if the system is a Mac
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    FONTFORGE_BIN="/Applications/FontForge.app/Contents/Resources/opt/local/bin/fontforge"
+    if [[ ! -x "$FONTFORGE_BIN" ]]; then
+        echo "FontForge binary not found in FontForge.app package."
+        exit 1
+    fi
+else
+    FONTFORGE_BIN="fontforge"
+fi
+
+# Run FontForge with the provided arguments
+$FONTFORGE_BIN -lang=ff -c 'Open($1); Generate($2)' sources/Vilnius.sfd sources/Vilnius-Regular.ufo
+
+# Dirty hack to add the openTypeOS2Selection key to the fontinfo.plist file
+FONTINFO=`cat sources/Vilnius-Regular.ufo/fontinfo.plist`
+echo "${FONTINFO/    <key>openTypeOS2Type<\/key>/    <key>openTypeOS2Selection</key>$'\n'    <array>$'\n'      <integer>7</integer>$'\n'    </array>$'\n'    <key>openTypeOS2Type</key>}" > sources/Vilnius-Regular.ufo/fontinfo.plist
